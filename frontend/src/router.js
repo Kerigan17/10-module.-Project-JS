6b4fe35ad8a3675c18f59dd535198556d09012bc
Kerigan17 <104769289+Kerigan17@users.noreply.github.com>
2023-06-27 20:00:13 +0300

    app and router 1

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,42 @@
+import {Registration} from "./components/registration.js";
+
 export class Router {
     constructor() {
         this.routs = [
             {
                 route: '#/',
                 title: 'Главная',
-                template: 'templates/home.html'
-            }
+                template: 'templates/home.html',
+                styles: 'css.home.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/registration',
+                title: 'Регистрация',
+                template: 'templates/registration.html',
+                styles: 'css/sign-in.css',
+                load: () => {
+                    new Registration()
+                }
+            },
         ]
+    }
+
+    async openRoute() {
+        const newRoute = this.routs.find(item => {
+            return item.route === window.location.hash;
+        })
+
+        if (!newRoute) {
+            window.location.href = '#/';
+            return;
+        }
+
+        document.getElementById('content').innerHTML = await fetch(newRoute.template).then(response => response.text());
+        document.getElementById('styles').setAttribute('href', newRoute.styles);
+        document.getElementById('title').innerText = newRoute.title;
+        newRoute.load();
     }
 }
